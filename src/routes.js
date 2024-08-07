7735482a6b05c3c05343b587bc073077046c10bb
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2024-08-06 21:03:21 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,7 @@ import SelecaoController from '../src/app/controllers/SelecaoController.js'
 const router = Router()
 router.get('/wrestlers', SelecaoController.index)
 router.get('/wrestlers/:id', SelecaoController.show)
-
+router.get('/', (req,res) => {
+    return res.json("hello world");
+});
 export default router
